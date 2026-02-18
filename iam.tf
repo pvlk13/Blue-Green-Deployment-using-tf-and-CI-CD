3f2ea208890597835e532b9821379f7dd7305814
VijayaLakshmi <vijayalakshmi.kp@gmail.com>
2026-02-18 03:52:56 +0100

    added code

--- iam.tf
+++ iam.tf
@@ -2,7 +2,7 @@ resource "aws_iam_role" "test_role" {
   name = "eb-instance-role"
 
   # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
+  
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
