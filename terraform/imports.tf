dd2ba3069a6897fe312a27e6fd75040873c800de
VijayaLakshmi <vijayalakshmi.kp@gmail.com>
2026-02-18 16:17:36 +0100

    Add EB environment imports

--- terraform/imports.tf
+++ terraform/imports.tf
@@ -7,3 +7,13 @@ import {
   to = aws_s3_bucket.eb_artifacts
   id = "vijaya-eb-artifacts-272183979798"
 }
+
+import {
+  to = aws_elastic_beanstalk_environment.blue-tf
+  id = "app-blue-tf"
+}
+
+import {
+  to = aws_elastic_beanstalk_environment.green-tf
+  id = "app-green-tf"
+}
